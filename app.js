f88da3491bc81866f5df0775862bd4916f6de992
Ramesh Vyas <rameshovyas@gmail.com>
2022-11-20 14:04:59 +0530

    Created view for sign up page and added crypto, bcrypt and mysql modules

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 /* Using dependent modules here */
 const express = require("express");
 const bodyParser = require("body-parser");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+const mysql = require("mysql");
 
 const PORT = 3000;
+
+// Create connection pool for my sql database
+const pool = mysql.createPool({
+    host     : 'localhost',
+    user     : 'root',
+    password : '',
+    database : 'QACommunity'
+});
+
 
 /*Creating express app */
 const app = express();
@@ -12,6 +24,10 @@ app.set('view engine','ejs');
 
 /* static middleware for rendering static files */
 app.use(express.static('public'));
+
+//Use body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 /* The root route for get request */
 app.get("/", (req,res) => {
@@ -32,6 +48,23 @@ app.get("/contact",(req,res)=> {
 app.get("/blog",(req,res)=> {
     res.render("blog")
 });
+
+/* GET/signup */
+app.get("/signup", (req,res) =>{
+    res.render("signup") 
+})
+
+/**
+ * Authentication Service Methods Here
+ */
+
+/* POST Sign up */
+app.post("/signup", (req,res) => {
+    res.render("thanks", {name:"Ramesh Vyas"})
+})
+
+
+
 
 /*Spin the server here */
 app.listen(PORT, ()=>{
